2ce23fc217b0a3bb23ef51517dda0134aeff29cc
Acute-Tri-angle <pptran106@student.hbuhsd.edu>
2024-09-11 10:31:27 -0700

    hhfisdfosfhfshdfsdf

--- Problem3.java
+++ Problem3.java
@@ -1,14 +1,15 @@
 public class Problem3{
     public static void main(String[] args){
         double decimal = 69.0182031;
+        System.out.println("decimal number: " + decimal);
         decimal = decimal * 10;
         int x = (int) decimal % 10;
-        System.out.println(x);
+        System.out.print("answer: " + x);
         decimal = decimal * 10;
          x = (int) decimal % 10;
-        System.out.print(x);
+        System.out.print(" " + x);
         decimal = decimal * 10;
          x = (int) decimal % 10;
-        System.out.print(x);
+        System.out.print(" "+ x);
     }
 }
